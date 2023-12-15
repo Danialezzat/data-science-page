6a3c52fbd56949bdede44a62743f748c48750c7b
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2023-12-15 01:23:52 -0800

    pricing section done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './HomeStyles.css';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Pricing from './Pricing';
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Hero />
+        <Pricing />
     </div>
   )
 }
---
+++ src/components/Pricing.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './PricingStyle.css';
+
+const Pricing = () => {
+  return (
+    <div className='pricing'>
+        <div className='container'>
+            <div className='top-content'>
+                <div>
+                    <h2>Everything you need for <span>$99 a month</span></h2>
+                    <p>Includes every featurenwe offer plus unlimited projects and unlimited users .</p>
+                </div>
+                <div className='btn-div'>
+                    <button>get started today</button>
+                </div>
+            </div>
+            <div className='button-content'>
+                <div className='card'>
+                    <p><span>Everything you need</span></p>
+                    <h3>All-in-one platform</h3>
+                    <p>
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                    </p>
+                </div>
+                <div className='card'>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                </div>
+                <div className='card'>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                    <div>
+                        <p>lorem is awesome for creating</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Pricing
