645a7bfc042000b4753137d38c04e6327e5cb0ae
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2023-12-15 04:22:09 -0800

    done with faq

---
+++ src/components/Faq.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './FaqStyle.css'
+
+const Faq = () => {
+  return (
+    <div className='section-faq'>
+        <div className='container'>
+            <div className='col-1'>
+                <h3>Frequently asked questions</h3>
+                <p>Can't fint the answer you are looking for</p>
+                <p>Reach out to our <span>customer support</span> team.</p>
+            </div>
+
+            <div className='col-2'>
+                <div className='faq'>
+                    <h5>How do you make holy water?</h5>
+                    <p>you biol the hell out of itlorem is out there to be.</p>
+                </div>
+                <div className='faq'>
+                    <h5>How do you make holy water?</h5>
+                    <p>you biol the hell out of itlorem is out there to be.</p>
+                </div>
+                <div className='faq'>
+                    <h5>How do you make holy water?</h5>
+                    <p>you biol the hell out of itlorem is out there to be.</p>
+                </div>
+                <div className='faq'>
+                    <h5>How do you make holy water?</h5>
+                    <p>you biol the hell out of itlorem is out there to be.</p>
+                </div>
+                <div className='faq'>
+                    <h5>How do you make holy water?</h5>
+                    <p>you biol the hell out of itlorem is out there to be.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Faq
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Pricing from './Pricing';
 import Testimonials from './Testimonials';
+import Faq from './Faq';
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
         <Hero />
         <Pricing />
         <Testimonials />
+        <Faq />
     </div>
   )
 }
